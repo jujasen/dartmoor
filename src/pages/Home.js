3c07e776d3f5f7280c851a11c3e2ec9ffc4fc65a
Julz <jujasen@hotmail.com>
2021-06-01 22:59:10 +0200

    added home landing section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
+import { Icon } from 'semantic-ui-react'
+
 const Home = () => {
     
 
     return (
         <>
-            <h1>home</h1>
+            <div className="page">
+                <section className="padded p--b p--t landing f f--col f--j-space">
+                    <h1 className="landing__title">Nature at its best</h1>
+                    <div className="f f--j-center f--a-center">
+                        <p className="landing__info m--r-s">scroll</p>
+                        <Icon size="large" className="block landing__icon" name="hand point down outline" />
+                    </div>
+                    
+                </section>
+
+            </div>
         </>
     )
 }

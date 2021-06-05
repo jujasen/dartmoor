dc47c8a1db9fe307bb7d46f55c4b2e95ffd4fbe3
Julz <jujasen@hotmail.com>
2021-06-05 19:43:36 +0200

    added info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,36 +7,40 @@ const Footer = () => {
 
 
     return (
-        <footer className="footer padded flex flex--j-space">
-            <img className="footer__logo" src={Logo} alt="Dartmoor" />
-            <nav className="f f--j-center">
-                <div className="m--r">
-                    <Link className="footer__link footer__link--main" to='/'>
-                        Home
+        <footer className="footer padded">
+            <div className="flex flex--j-space flex--a-center">
+                <img className="footer__logo" src={Logo} alt="Dartmoor" />
+                <nav className="f f--j-center">
+                    <div className="m--r">
+                        <Link className="footer__link footer__link--main" to='/'>
+                            Home
                 </Link>
-                    <Link className="footer__link footer__link--main" to='/info/general'>
-                        Information centres
+                        <Link className="footer__link footer__link--main" to='/info/general'>
+                            Information centres
                 </Link>
-                </div>
-                <div>
-                    <p className="footer__link footer__link--main">Places</p>
-                    <Link className="footer__link" to='/places/tavistock/general'>
-                        Tavistock
+                    </div>
+                    <div>
+                        <p className="footer__link footer__link--main">Places</p>
+                        <Link className="footer__link" to='/places/tavistock/general'>
+                            Tavistock
                     </Link>
-                    <Link className="footer__link" to='/places/postbridge/general'>
-                        Post Bridge
+                        <Link className="footer__link" to='/places/postbridge/general'>
+                            Post Bridge
                     </Link>
-                    <Link className="footer__link" to='/places/okehampton/general'>
-                        Okehampton
+                        <Link className="footer__link" to='/places/okehampton/general'>
+                            Okehampton
                     </Link>
-                    <Link className="footer__link" to='/places/widecombe/general'>
-                        Widecombe
+                        <Link className="footer__link" to='/places/widecombe/general'>
+                            Widecombe
                     </Link>
-                    <Link className="footer__link" to='/places/buckfastleigh/general'>
-                        Buckfastleigh
+                        <Link className="footer__link" to='/places/buckfastleigh/general'>
+                            Buckfastleigh
                     </Link>
-                </div>
-            </nav>
+                    </div>
+                </nav>
+            </div>
+            <p className="footer__info">All images from <a href="https://pixabay.com/">Pixabay</a></p>
+
         </footer>
     )
 }
